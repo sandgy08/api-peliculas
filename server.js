84bf6b612f88a7ff6ea1a37a27e83184c836c043
SandGy <gordillo.sandy08@gmail.com>
2026-05-27 15:01:15 -0700

    Configuracion Render

--- server.js
+++ server.js
@@ -136,8 +136,10 @@ sequelize.sync({ force: true }).then(async () => {
         }
     ]);
 
-    app.listen(3000, () => {
-        console.log('Servidor corriendo en puerto 3000');
-    });
+   const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
 
 });
